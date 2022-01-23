ec9188157c2b2586e99df05deda44b5e308f81ca
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2022-01-22 21:56:12 -0500

    Comments redone and updated

---
+++ models/Comment.js
@@ -0,0 +1,53 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {};
+  
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: true,
+      validate: {
+          model: 'User',
+          key: 'id'
+      }
+    },
+    post_id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'post',
+            key: 'id'
+        }
+    },
+    date_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    }
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+}
+
+);
+
+module.exports = Comment;
